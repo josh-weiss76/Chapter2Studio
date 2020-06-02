75b13875a15d7462f28ea233f2cc86d971141e24
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-02 14:14:46 -0400

    updated code with final bonus mission

--- Chapter2Studio/Program.cs
+++ Chapter2Studio/Program.cs
@@ -7,20 +7,34 @@ namespace Chapter2Studio
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter the radius of a circle in miles: ");
-            float radiusInput = float.Parse(Console.ReadLine());
-            
-            while (radiusInput <= 0)
+            //float radiusInput = float.Parse(Console.ReadLine());
+            string userInput = Console.ReadLine();
+            float radiusInput;
+            while (!float.TryParse(userInput, out radiusInput) || (radiusInput <= 0))
             {
-                Console.WriteLine("Please enter a positive value for the radius: ");
-                radiusInput = float.Parse(Console.ReadLine());
-            }
+                Console.WriteLine("Invalid entry, the radius should be a positive numeric character. Please enter the radius of a circle in miles: ");
+                userInput = Console.ReadLine();
+            } 
+
+            //while (radiusInput <= 0)
+            //{
+            //    Console.WriteLine("Invalid entry, the radius should be a positive numeric character.  Please enter the radius of a circle in miles: ");
+            //    radiusInput = float.Parse(Console.ReadLine());
+            //}
             //Console.WriteLine("The radius you entered is: " + radiusInput);
             //Console.WriteLine(radiusInput.GetType());
             Console.WriteLine("The area of a circle with a radius of " + radiusInput + " is " + CircleMeasurements.CircleArea(radiusInput));
             Console.WriteLine("The circumference of the circle is " + CircleMeasurements.CircleCircumference(radiusInput));
             Console.WriteLine("The diameter of the circle is " + CircleMeasurements.CircleDiameter(radiusInput));
-            Console.WriteLine("Please enter the miles per gallon (mpg) of your vehicle: ");
-            float mpgInput = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the miles per gallon (MPG) of your vehicle: ");
+            string userMPG = Console.ReadLine();
+            float mpgInput;
+            while (!float.TryParse(userMPG, out mpgInput) || (mpgInput <= 0))
+            {
+                Console.WriteLine("Invalid entry, the MPG should be a positive numeric character. Please your vehicle MPG: ");
+                userMPG = Console.ReadLine();
+            }
+
             Console.WriteLine("With a mpg of " + mpgInput + " your vehicle will use " + Roadtrip.GallonsPerTrip(CircleMeasurements.CircleCircumference(radiusInput), mpgInput) + " gallons of gasoline to travel the circumference of the circle");
         }
     }
